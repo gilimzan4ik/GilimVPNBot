f3469420fbac4a912a75b64b8889526badf42a98
gilimzan4ik <karymovamir@gmail.com>
2026-07-21 20:55:23 +0500

    An approximate structure for the admin panel

--- AdminVPNBot.py
+++ AdminVPNBot.py
@@ -2,12 +2,9 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 import json
 import os
-from datetime import datetime, timedelta
 
 data_file = "database.json"
 token_file = "token_admin.txt"
-admin_id_file = "admin_id.txt"
-waiting_config = None
 
 def load_data():
     if os.path.exists(data_file):
@@ -23,14 +20,45 @@ def read_token():
     with open(token_file, "r", encoding="utf-8") as f:
         return f.readline()
 
-def admin_id():
-    with open(admin_id_file, "r", encoding="utf-8") as f:
-        return int(f.readline())
-
-admin_id = admin_id()
-
 chat_data = load_data()
-
 token = read_token()
-
 bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(
+        message.chat.id,
+        f"Привет, это админка сервиса GilimVPN.\n\nЕсли ты тут сидишь, значит ты избранный",
+        reply_markup=get_main_keyboard()
+    )
+
+def get_main_keyboard():
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row(KeyboardButton("Пользователи"))
+    keyboard.row(KeyboardButton("Статистика"))
+    keyboard.row(KeyboardButton("Помощь"))
+    return keyboard
+
+@bot.message_handler(func=lambda message: message.text == "Пользователи")
+def users_message(message):
+    bot.send_message(
+        message.chat.id,
+        "Тут как-нибудь надо сделать вывод всех пользователей с их ником и со сроком действия подписки"
+    )
+
+@bot.message_handler(func=lambda message: message.text == "Пользователи")
+def users_message(message):
+    bot.send_message(
+        message.chat.id,
+        "А сюда всю какую никакую статистику написать"
+    )
+
+@bot.message_handler(func=lambda message: message.text == "Помощь")
+def help_message(message):
+    bot.send_message(
+        message.chat.id,
+        "За помощью или обратной связью сюда: @fancutedora"
+    )
+
+
+bot.infinity_polling()
